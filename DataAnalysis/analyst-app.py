315cd18d211528927f2780d5c7c976a98b411f90
Edward Twumasi <edward.twumasi@hubtel.com>
2023-09-24 10:51:57 +0000

    build webapp with streamlit.Null values and data decription feature

--- DataAnalysis/analyst-app.py
+++ DataAnalysis/analyst-app.py
@@ -65,18 +65,20 @@ def read_input(source):
 # summariise data
 def describe_data(data):
     if len(data) > 0:
+        """ ###
+        Shape of dataset 
+        """
+        #Get shape of data
+        shape = data.shape
+
+        st.write(" ###### Dataset has {} rows and {} columns".format(shape[0],shape[1])) 
+       
         # Take a peep at the data
-        """ ### Descripe your data
+        """ ### Explore your data
         View first 5 rows
         """
 
         st.write(data.head())
-
-        """ ###
-        Datatypes in dataset 
-        """
-        #Get data types
-        st.write(data.dtypes)
 
         """ ###
         Columns in dataset
@@ -85,19 +87,72 @@ def describe_data(data):
         st.write(data.columns)
 
         """ ###
-        Shape of dataset 
+        Datatypes in dataset 
         """
-        #Get shape of data
-        shape = data.shape
+        #Get data types
+        st.write(data.dtypes)
+        
+        numerical_dtypes = ['int64', 'float64']  
 
-        st.write("Dataset has {} rows and {} columns".format(shape[0],shape[1])) 
-       
+        if all(dt in numerical_dtypes for dt in data.dtypes):
+            st.write("Dataset contains only numerical data types")
+            """ ###
+           Summary  
+             """
+            st.write(data.describe())
+        else:
+            st.write(""" #### Dataset contains non-numerical data types""")
 
-        """ ###
-          Describe dataset 
-        """
-        #Descriptive summary
-        st.write(data.describe())
+            """ ###
+            Summary of numerical fields
+            """
+            #Descriptive summary
+            st.write(data.describe())
+
+            """ ###
+            Summary of categorical fields
+            """
+            #Descriptive summary
+            st.write(data.select_dtypes(exclude=['int64','float64']).describe())
+
+
+
+#What fields have  null values
+
+def get_null_stats(data):
+    
+    """ ###
+         Missing values in dataset
+    """
+    #What is the percentage of these null values 
+    null_fields = get_null_fields(data)[0]
+
+    if len(null_fields) == 0:
+        st.write("Dataset has no missing values")
+        return
+
+    null_fields_names = get_null_fields(data)[1]
+
+
+    st.write(null_fields)
+    
+    for col in null_fields_names:
+        null_fields[col] = null_fields[col]*100/len(data)
+
+    null_fields = null_fields.rename("Missing Values %")
+    st.write("(n_null/n_rows) % for each column")
+    st.write(null_fields)
+
+
+def get_null_fields(data):
+    null_stats = data.isnull().sum()
+    null_fields = null_stats[null_stats>0]
+
+    null_fields = null_fields.rename("Missing Values")
+    #What is the percentage of these null values 
+    null_fields_names = null_fields.index
+
+    return (null_fields,null_fields_names)
 
 
 def plot_data(data):
@@ -123,5 +178,7 @@ if file:
         data = input[0]
        
         describe_data(data)
+
+        get_null_stats(data)
 
        
