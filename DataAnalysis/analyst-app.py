c6feed998bd5306e685685002a0a276d8db97f8f
Edward Twumasi <edward.twumasi@hubtel.com>
2023-09-27 01:40:33 +0000

    build webapp with streamlit.Missing data imputation and categorical encoding

--- DataAnalysis/analyst-app.py
+++ DataAnalysis/analyst-app.py
@@ -134,15 +134,21 @@ def get_null_stats(data):
         Missing values in dataset
     """)
     
-    st.write(null_fields)
+    left_column, right_column = st.columns(2)
+
+    with left_column:
+        st.write(null_fields)
     
     for col in null_fields_names:
         null_fields[col] = null_fields[col]*100/len(data)
 
     null_fields = null_fields.rename("Missing Values %")
-    st.write("(n_null/n_rows) % for each column")
-    st.write(null_fields)
-    suggest_cleaning_options()
+
+    with right_column:
+        st.write(null_fields)
+        st.write("n_null/n_rows % per column")
+    
+    
 
 
 def get_null_fields(data):
@@ -158,11 +164,74 @@ def get_null_fields(data):
 def suggest_cleaning_options():
         st.write("##### Possible actions to take")
         st.write("""
-            1. Drop rows with any missing values
-            2. Drop columns with missing values
-            3. Inpute missing data
+            1. Drop columns with missing values
+            2. Inpute missing data
             """)
-   
+        
+def clean_data(data):
+
+    null_fields = get_null_fields(data)[0]
+
+    if len(null_fields) > 0:
+
+        suggest_cleaning_options()
+
+        action = st.radio(
+        'Preferred action',
+        ("Impute missing","Drop columns"))
+        st.write(f"Action . {action} ")
+
+        cleaned_data = take_action(action,data,get_null_fields(data)[1])
+
+        st.write(cleaned_data)
+
+
+
+def impute_missing(data):
+    from sklearn.impute import SimpleImputer
+
+    # Imputation
+    my_imputer = SimpleImputer()
+    imputed_X_train = pd.DataFrame(my_imputer.fit_transform(data))
+
+    # Imputation removed column names; put them back
+    imputed_X_train.columns = data.columns
+    return imputed_X_train
+
+def encode_categorical(data):
+
+    object_cols = data.select_dtpyes(exclude = ['int64','float64']).columns
+
+    from sklearn.preprocessing import OneHotEncoder
+
+    # Apply one-hot encoder to each column with categorical data
+    OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+    OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(data[object_cols]))
+
+    # One-hot encoding removed index; put it back
+    OH_cols_train.index = data.index
+
+    # Remove categorical columns (will replace with one-hot encoding)
+    num_X_train = data.drop(object_cols, axis=1)
+
+    # Add one-hot encoded columns to numerical features
+    OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+
+    # Ensure all columns have string type
+    OH_X_train.columns = OH_X_train.columns.astype(str)
+
+def take_action(decision,data,columns=[]):
+    
+    if decision == "Drop columns":
+       
+       cleaned = data.drop(columns,axis=1)
+       st.write("Dropped columns {} from dataset".format(columns))
+
+    elif decision == "Impute missing":
+
+        impute_missing(data)
+
+    return cleaned
 
 def plot_data(data):
      st.write("Plotting columns {}".format(data.columns[1]))
@@ -189,5 +258,10 @@ if file:
         describe_data(data)
 
         get_null_stats(data)
+
+        clean_data(data)
+
+
+        
 
        
